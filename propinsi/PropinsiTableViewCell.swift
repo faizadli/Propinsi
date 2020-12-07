ce9a39224c9ef511c4f631afb4ccbb0cf0c753d8
faizadli <68326914+faizadli@users.noreply.github.com>
2020-12-07 15:36:38 +0700

    Add files via upload

---
+++ propinsi/PropinsiTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PropinsiTableViewCell.swift
+//  propinsi
+//
+//  Created by faiz on 12/11/20.
+//
+
+import UIKit
+
+class PropinsiTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var propinsiView: UIView!
+    @IBOutlet weak var propinsiImg: UIImageView!
+    @IBOutlet weak var propinsiLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
